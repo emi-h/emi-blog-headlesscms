3bce515547f45feb86c7fead6c7c638f7750f3df
emih <emi.programming@gmail.com>
2022-07-01 13:56:54 +0900

    first commit

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # Next.js + TypeScript + Tailwind CSS + ESLint + Prettier
 
-```bash
-yarn create next-app --example https://github.com/lightsound/Next.js-TypeScript-TailwindCSS-ESLint-Prettier
-```
+blog site 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,35 @@
-import type { NextPage } from "next";
+import { MicroCMSListResponse } from "microcms-js-sdk";
+import type { GetStaticProps, NextPage } from "next";
+import Link from "next/link";
+import { client } from "src/libs/client";
 
-const Home: NextPage = () => {
-  return <div className="text-blue-500">Hello!</div>;
+export type Blog ={
+  title:string;
+  content:string;
+}
+
+const Home: NextPage<MicroCMSListResponse<Blog>> = (props) => {
+  return <div>
+    <p className="text-gray-400">{`Number of articles：${props.totalCount}件`}</p>
+    <ul className="mt-4 space-y-4">
+      {props.contents.map((content)=>{
+        return(
+          <li key={content.id}>
+            <Link href={`/blog/${content.id}`}>
+              <a className="underline text-xl text-blue-800 hover:text-blue-400">{content.title}</a>
+            </Link>
+          </li>
+        )
+      })}
+    </ul>
+  </div>;
+};
+
+export const getStaticProps:GetStaticProps<MicroCMSListResponse<Blog>>=async()=>{
+  const data = await client.getList<Blog>({endpoint: 'blogs'},);
+  return{
+    props:data,
+  };
 };
 
 export default Home;
