013971a68575958ad38a56d3c55b1db2b5715d56
emih <emi.programming@gmail.com>
2022-07-02 15:18:18 +0900

    ✨ Add search box

---
+++ src/pages/api/search.ts
@@ -0,0 +1,11 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { client } from 'src/libs/client';
+import { Blog } from 'src/pages';
+
+const handler= async(req: NextApiRequest, res: NextApiResponse) => {
+    const data = await client.getList<Blog>({endpoint: 'blogs',queries:{q:req.body.q}});
+    res.status(200).json(data);
+};
+
+
+export default handler;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { MicroCMSListResponse } from "microcms-js-sdk";
 import type { GetStaticProps, NextPage } from "next";
 import Link from "next/link";
+import { ComponentProps, useState } from "react";
 import { client } from "src/libs/client";
 
 export type Blog ={
@@ -9,24 +10,53 @@ export type Blog ={
 }
 
 const Home: NextPage<MicroCMSListResponse<Blog>> = (props) => {
-  return <div>
-    <p className="text-gray-400">{`Number of articles：${props.totalCount}`}</p>
+const [search, setSearch]= useState<MicroCMSListResponse<Blog>>();
+
+  const handleSubmit:ComponentProps<"form">["onSubmit"] = async (event)=>{
+    event.preventDefault();
+    const q = event.currentTarget.query.value;
+    const data =await fetch("/api/search",{
+      method:"POST",
+      headers:{"Content-type":"application/json"},
+      body:JSON.stringify({q}),
+    });
+    const json:MicroCMSListResponse<Blog> = await data.json();
+    setSearch(json);
+  };
+
+  const handleReset:ComponentProps<"button">["onClick"]=()=>{
+    setSearch(undefined);
+  };
+  
+  const contents = search ? search.contents : props.contents;
+  const totalCount = search ? search.totalCount : props.totalCount;
+
+  return (
+  <div>
+     <form onSubmit={handleSubmit}>
+      <input className="border border-black px-2" type="text" name="query" />
+      <button className="border border-black px-2">Search</button>
+      <button type="reset" className="border border-black px-2" onClick={handleReset}>Reset</button>
+    </form>
+    <p className="text-gray-400">{`${search ? "Number of articles" : "Total article"}：${totalCount}`}</p>
     <ul className="mt-4 space-y-4">
-      {props.contents.map((content)=>{
+      {contents?.map((content)=>{
         return(
           <li key={content.id}>
             <Link href={`/blog/${content.id}`}>
-              <a className="underline text-xl text-blue-800 hover:text-blue-400">{content.title}</a>
+              <a className="text-xl hover:text-blue-400">{content.title}</a>
             </Link>
           </li>
         )
       })}
     </ul>
-  </div>;
+  </div>
+  )
+  ;
 };
 
 export const getStaticProps:GetStaticProps<MicroCMSListResponse<Blog>>=async()=>{
-  const data = await client.getList<Blog>({endpoint: 'blogs'},);
+  const data = await client.getList<Blog>({endpoint: 'blogs'});
   return{
     props:data,
   };
