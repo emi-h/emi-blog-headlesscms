89413f601123a1a4406123562b72e4dc62f9ccd7
emih <emi.programming@gmail.com>
2022-07-05 20:45:58 +0900

    Add ESlink setting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,16 @@
 module.exports = { 
+    "env":{
+        "browser":true,
+        "es2022":true,
+        "node":true,
+    },
+    "parserOptions":{
+        "sourceType":"module"
+    },
     "extends": ["next/core-web-vitals", "prettier"],
     "rules":{
         "semi":["error","always"],
-        "quotes":["error","double"]
+        "quotes":["error","double"],
+        "no-undef":"error"
     }
 };
