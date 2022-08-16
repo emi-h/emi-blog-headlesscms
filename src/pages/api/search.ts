47c19534af382b936cad94d883f69fe9fc2bd39f
emih <emi.programming@gmail.com>
2022-08-16 17:48:01 +0900

    🐛

--- src/pages/api/search.ts
+++ src/pages/api/search.ts
@@ -1,9 +1,9 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { client } from 'src/libs/client';
-import { Blog } from 'src/pages';
+import type { NextApiRequest, NextApiResponse } from "next";
+import { client } from "src/libs/client";
+import { Blog } from "src/pages";
 
 const handler= async(req: NextApiRequest, res: NextApiResponse) => {
-    const data = await client.getList<Blog>({endpoint: 'blogs',queries:{q:req.body.q}});
+    const data = await client.getList<Blog>({endpoint: "blogs",queries:{q:req.body.q}});
     res.status(200).json(data);
 };
 
